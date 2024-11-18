3fa1f30f064a37c588c52e970ce449201eaf3ff7
webnetkz <dbqqbq@gmail.com>
2024-11-19 00:41:47 +0500

    Root commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+  <script src="./assets/main.js"></script>
+  <script>
+			if('serviceWorker' in navigator)
+			{
+				// window.addEventListener('load', function() {
+				// 	navigator.serviceWorker.register('./sw.js')
+				// 	.then(function(registration) {
+				// 	})
+				// 	.catch(function(error) {
+				// 	});
+				// });
+			}
+		</script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once './settings.php';
+
+?>
+
+<html lang="en">
+    <head>
+        <title><?=$title;?></title>
+
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="#FFF">
+        <meta name="author" content="webnet.kz">
+        <meta name="description" content="Лидер на рынке Казахстана по производству и монтажу перил различной конфигурации и сложности">
+        <meta name="keywords" content="Перила в Алматы, Перила, Перила в Астане, Балясины, Балясины в Алмате">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width">
+        <meta name="robots" content="index, follow">
+
+        <link rel="stylesheet" href="./assets/styles.css">
+
+        <link rel="shortcut icon" type="image/png" href="./assets/logo512.png">
+        <link rel="apple-touch-icon" href="./assets/logo512.png">
+        <link rel="manifest" href="./manifest.json"> 
+      </head>
+    <body>
